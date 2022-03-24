9728b2fb2d4bfa4650854eed0dccde56b43a54b1
benng <benng822@gmail.com>
2022-03-24 16:35:05 +0800

    ch5 created user management function, user are able to login, logout, signup

---
+++ ch5-user-registration/accounts/forms.py
@@ -0,0 +1,18 @@
+from dataclasses import field
+from webbrowser import get
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
